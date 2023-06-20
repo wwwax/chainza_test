05603a3223af66d7b61e03e6c048884340b661ca
Max Ivanov <waxivanov@gmail.com>
2023-06-20 11:21:53 +0300

    feat: add mui

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,22 +3,26 @@ import { AppProps } from 'next/app';
 import { Provider } from 'react-redux';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { ReactQueryDevtools } from 'react-query/devtools';
+import { ThemeProvider } from '@mui/material/styles';
 
 import GlobalStyle from '@/components/GlobalStyle';
 
 import { store } from '@/redux/store';
+import theme from '@/utils/theme';
 
 const queryClient = new QueryClient();
 
 const App = ({ Component, pageProps }: AppProps): JSX.Element => {
   return (
-    <QueryClientProvider client={queryClient}>
-      <Provider store={store}>
-        <GlobalStyle />
-        <Component {...pageProps} />
-      </Provider>
-      <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>
+    <ThemeProvider theme={theme}>
+      <QueryClientProvider client={queryClient}>
+        <Provider store={store}>
+          <GlobalStyle />
+          <Component {...pageProps} />
+        </Provider>
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
+    </ThemeProvider>
   );
 };
 
